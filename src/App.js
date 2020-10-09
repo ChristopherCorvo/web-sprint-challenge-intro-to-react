8cbe616748aa1eba06f378272a0cacfbe23b6ec0
Christopher Corvo <christophercorvo@gmail.com>
2020-10-09 12:20:03 -0400

    Have all the names and birth year on the page from starwars API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'; // allow for the import from starwars api
+
+// Importing components 
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +13,22 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [ characterData, setCharacterData] = useState([]) // adding a slice of state i.e starwars characters
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacterData(res.data.results)
+      })
+      .catch(err =>{
+        console.log('Error getting data from StarWars API')
+      })
+  },[])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Characters</h1> {/* get rid of this div later  */}
+      <Character characterData = {characterData}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
-// Write your Character component here
+import React from 'react';
+
+
+const Character = props => {
+
+    const {characterData} = props // array of objects consisting of character names 
+    console.log(characterData)
+
+    
+    return (
+        <div>
+            {
+                characterData.map((char) => {
+                    return <div> 
+                                <h1>{char.name}</h1>
+                                <h2>{char.birth_year}</h2> 
+                            </div>
+                })
+            }
+        </div>
+    )
+
+    
+}
+
+
+export default Character;
