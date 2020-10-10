bb8d54ae8bebb5c174fd4517edf16478f1be6a59
Christopher Corvo <christophercorvo@gmail.com>
2020-10-10 10:23:21 -0400

    adding dropdown menu for character info on character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,12 +6,11 @@ const MainCharcterContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    
-
 `
 
 const CharacterInfo = styled.div`
     display:flex;
+    flex-direction: column;
     justify-content:space-between;
     border: 2px solid red;
     width: 50%;
@@ -20,34 +19,62 @@ const CharacterInfo = styled.div`
     padding-left: 1%;
     padding-right: 1%;
     background-color: red;
-
+    border: 3px outset yellow;
+    
     &:hover{
-        box-shadow: 4px 3px 4px red ;
+        box-shadow: 10px 10px 10px red ;
     }
 
     h1 {
         color: white;
     }
 
-    h2 {
-        color:white;
+    .dropdown-content {
+        display: none;
+        flex-direction: column;
+        /* position: absolute; */
+        background-color: red;
+        width: 100%;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+        color: white;
+    }
+
+    .show {display:flex;}
+`
+    function myFunction() {
+    
+        // document.getElementById("myDropdown").classList.toggle("show");
+
+        const dropDown = document.getElementsByClassName('myDropdown')
+
+        for (let i = 0; i < dropDown.length; i++) {
+            dropDown[i].classList.toggle('show')
+            
+        }
     }
     
-`
-
 const Character = props => {
 
     const {characterData} = props // array of objects consisting of character names 
     console.log(characterData)
 
-    
+   
     return (
         <MainCharcterContainer>
             {
                 characterData.map((char) => {
-                    return <CharacterInfo> 
+                    return <CharacterInfo onClick={myFunction} > 
                                 <h1>{char.name}</h1>
-                                <h2>{char.birth_year}</h2> 
+                                <div className="dropdown-content myDropdown">
+                                    <div><h3>Gender: {char.gender}</h3></div>
+                                    <div><h3>Height: {char.height}</h3></div>
+                                    <div><h3>Mass: {char.mass}</h3></div>
+                                    <div><h3>DOB: {char.birth_year}</h3></div>
+                                    <div><h3>Eye Color: {char.eye_color}</h3></div>
+                                    <div><h3>Hair Color: {char.hair_color}</h3></div> 
+                                </div>
+                                
                             </CharacterInfo>
                 })
             }
@@ -56,6 +83,7 @@ const Character = props => {
 
     
 }
+
 
 
 export default Character;
